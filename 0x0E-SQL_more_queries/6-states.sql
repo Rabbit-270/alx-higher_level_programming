40d8482ae77488a74af6af2f161a1298281ae807
APINDA <apinda127@gmail.com>
2024-05-22 11:17:36 -0700

    Add states to new database

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Creates a database and a table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+id INTEGER PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
+name VARCHAR(256) NOT NULL
+);
