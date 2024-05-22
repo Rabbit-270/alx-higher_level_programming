23b0a319d9d2e5e6982f4e49036285a59c071e7c
APINDA <apinda127@gmail.com>
2024-05-22 11:46:42 -0700

    Added.

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Show cities stored in a database
-SELECT id, name FROM cities WHERE id IN (SELECT id FROM states WHERE name='California');
+SELECT id, name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name='California');
