1519cb2a0bc12540827caa4528bda8f828a9e6a5
APINDA <apinda127@gmail.com>
2024-05-22 13:36:30 -0700

    Join states with the cities

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Shows all cities in the database
-SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states WHERE states.id=cities.id ORDER BY cities.id ASC;
+SELECT DISTINCT cities.id, cities.name, states.name FROM cities INNER JOIN states ON states.id=cities.state_id ORDER BY cities.id ASC;
