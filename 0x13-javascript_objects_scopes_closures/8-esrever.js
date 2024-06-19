84f1270be68258b76773b74d72ac052f5083fefb
APINDA <apinda127@gmail.com>
2024-06-19 15:57:43 -0700

    Reverse an array

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  let reversedArray = [];
+  const reversedArray = [];
   let indRA = 0;
   for (let ind = (list.length - 1); ind >= 0; ind--) {
     reversedArray[indRA] = list[ind];
