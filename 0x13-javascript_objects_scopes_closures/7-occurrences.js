8724d52231fd1fd7ac4dd45a40722bde5db4b081
APINDA <apinda127@gmail.com>
2024-06-19 15:39:16 -0700

    Occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-exports.nbOccurrences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let find = 0;
   for (let ind = 0; ind < list.length; ind++) {
     if (list[ind] === searchElement) {
