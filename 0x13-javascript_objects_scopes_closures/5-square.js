31bd554ae71b2141983e361c6e1fc40681a81e9c
APINDA <apinda127@gmail.com>
2024-06-19 14:10:40 -0700

    Square#0

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,11 +2,8 @@
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-  size;
-
   constructor (size) {
     super(size, size);
-    this.size = size;
   }
 }
 module.exports = Square;
