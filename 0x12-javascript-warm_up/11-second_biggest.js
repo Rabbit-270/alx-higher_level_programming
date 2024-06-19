8cdf641ae60073d952f22ace6eb6cdd9a6a3a578
APINDA <apinda127@gmail.com>
2024-06-19 03:52:47 -0700

    Find second biggest in a number array.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,21 @@
 #!/usr/bin/node
 
 function sort (a) {
-  return (a.sort());
+  for (let outer = 0; outer < a.length; outer++) {
+    let curr = a[outer];
+    for (let inner = (outer); inner <= (a.length - 1); inner++) {
+      if (curr > a[inner]) {
+        const temp = curr;
+        a[outer] = a[inner];
+        curr = a[outer];
+        a[inner] = temp;
+        inner = outer - 1;
+      }
+    }
+  }
+  console.log(a);
+
+  return (a);
 }
 const process = require('node:process');
 const ARG_COUNT = process.argv.length - 2;
@@ -11,6 +25,9 @@ if (ARG_COUNT === 0) {
   console.log(0);
 } else {
   const ARRAY = process.argv.slice(2);
+  for (let b = 0; b < ARRAY.length; b++) {
+    ARRAY[b] = Number(ARRAY[b]);
+  }
   const sortedArray = sort(ARRAY);
   const convertedValue = Number(sortedArray[sortedArray.length - 2]);
   console.log(convertedValue);
