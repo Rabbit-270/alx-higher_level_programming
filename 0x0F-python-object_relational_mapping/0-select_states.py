1c73a2628a60f45b1c78544dfe290c66ae31ad50
APINDA <apinda127@gmail.com>
2024-06-21 04:57:05 -0700

    ...

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,15 +7,15 @@ This script retrieves stored states from a mysql database.
 if __name__ == "__main__":
     ''' The above statement ensures that my script
     is not executed when it is imported '''
-    Uname = argv[1]
+    U_n = argv[1]
     Pass = argv[2]
-    dbName = argv[3]
-    hostName = "localhost"
-    port = 3306
-    db = MySQLdb.connect(host=hostName, user=Uname, passwd=Pass, db=dbName)
+    DB = argv[3]
+    Host = "localhost"
+    Port = 3306
+    db = MySQLdb.connect(host=Host, port=Port, user=U_n, passwd=Pass, db=DB)
 
     cur = db.cursor()
-    cur.execute('SELECT * FROM states')
+    cur.execute('SELECT * FROM states ORDER BY states.id ASC')
     states = cur.fetchall()
     for state in states:
         print(state)
