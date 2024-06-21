565817f8148e7f1b905c1ef5245851ecf1f89931
APINDA <apinda127@gmail.com>
2024-06-21 07:06:53 -0700

    User defined filter

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' Shows a state provided by a user '''
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    uN = argv[1]
+    pW = argv[2]
+    dN = argv[3]
+    Name = argv[4]
+    Host = "localhost"
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC"
+
+    conn = MySQLdb.connect(host=Host, port=3306, user=uN, passwd=pW, db=dN)
+    myCur = conn.cursor()
+    myCur.execute(query.format(Name))
+    state = myCur.fetchall()
+    for s in state:
+        print(s)
+    myCur.close()
+    conn.close()
