6e1fd3df38c42ebd9953b19ab71f31852efebb39
APINDA <apinda127@gmail.com>
2024-06-21 05:34:36 -0700

    0-select_states Fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
+'''
+Lists all states stored in a database
+'''
 import MySQLdb
 from sys import argv
-'''
-This script retrieves stored states from a mysql database.
-'''
-if __name__ == "__main__":
-    ''' The above statement ensures that my script
-    is not executed when it is imported '''
-    U_n = argv[1]
-    Pass = argv[2]
-    DB = argv[3]
-    Host = "localhost"
-    Port = 3306
-    db = MySQLdb.connect(host=Host, port=Port, user=U_n, passwd=Pass, db=DB, charset='utf8')
 
-    cur = db.cursor()
-    cur.execute('SELECT * FROM states ORDER BY states.id ASC')
+if __name__ == "__main__":
+    U = argv[1]
+    P = argv[2]
+    D = argv[3]
+    H = "localhost"
+    p = 3306
+
+    conn = MySQLdb.connect(host=H, port=p, user=U, passwd=P, db=D)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     states = cur.fetchall()
     for state in states:
         print(state)
     cur.close()
-    db.close()
+    conn.close()
