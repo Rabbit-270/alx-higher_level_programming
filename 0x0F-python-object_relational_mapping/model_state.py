92ab6b5424dd3a1cbafc7ce7653f011d39d0d30a
Rabbit-270 <135014716+Rabbit-270@users.noreply.github.com>
2024-06-23 01:34:29 +0200

    Update model_state.py, cellphone version

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
