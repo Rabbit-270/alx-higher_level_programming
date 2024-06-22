ac9a65bb23c62a0d67d76fd1a1906a138c79920a
APINDA <apinda127@gmail.com>
2024-06-22 14:07:26 -0700

    4-cities_by_state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     Hst = "localhost"
     prt = 3306
 
-    query = "SELECT cities.id, cities.name, states.name FROM cities RIGHT "
-    query = query + "JOIN states ON cities.state_id = states.id "
+    query = "SELECT cities.id, cities.name, states.name FROM cities, "
+    query = query + "states WHERE cities.state_id = states.id "
     query = query + "ORDER BY cities.id ASC"
 
     conn = MySQLdb.connect(host=Hst, user=usr, passwd=pss, db=dB, port=prt)
