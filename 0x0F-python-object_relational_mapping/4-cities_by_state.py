1f08002c4bc753241f28323cca923647e1c11646
APINDA <apinda127@gmail.com>
2024-06-22 14:01:48 -0700

    List city by state, ascending order.

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+List cities and their states.
+'''
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    usr = argv[1]
+    pss = argv[2]
+    dB = argv[3]
+    Hst = "localhost"
+    prt = 3306
+
+    query = "SELECT cities.id, cities.name, states.name FROM cities RIGHT "
+    query = query + "JOIN states ON cities.state_id = states.id "
+    query = query + "ORDER BY cities.id ASC"
+
+    conn = MySQLdb.connect(host=Hst, user=usr, passwd=pss, db=dB, port=prt)
+    cur = conn.cursor()
+    cur.execute(query)
+
+    data = cur.fetchall()
+    for state_city in data:
+        print(state_city)
+    cur.close()
+    conn.close()
