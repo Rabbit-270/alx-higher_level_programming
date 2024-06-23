fdb33511f4bec24f059d0b39f440d03a17495648
APINDA <apinda127@gmail.com>
2024-06-22 23:11:30 -0700

    ...

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+'''
+this module contains the foundational Base class
+and a State class used for mapping SQL table.
+'''
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
@@ -7,9 +11,9 @@ Base = declarative_base()
 
 
 class State(Base):
-    ''' State class maps to the
-    states table in my database.'''
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False,
-                unique=True)
+    '''
+    Maps the state table in hbtn_0e_6_usa database.
+    '''
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
